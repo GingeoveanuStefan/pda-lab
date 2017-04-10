1c54333bc66a3e986e12635ce3e07342c5d2e2f5
GingeoveanuStefan <stefan.razv@gmail.com>
2017-04-10 23:03:27 +0300

    Add files via upload

--- TEME/MPI/lab3 - matrix multiplication/source_MatrixMultiplication.cpp
+++ TEME/MPI/lab3 - matrix multiplication/source_MatrixMultiplication.cpp
@@ -7,40 +7,28 @@
 #define MASTER 0
 
 int **alloc_matrix(int rows, int cols) {
-    int *line = (int *)malloc(rows*cols*sizeof(int));
-    int **matrix= (int **)malloc(rows*sizeof(int*));
-    for (int i=0; i<rows; i++)
-        matrix[i] = &(data[cols*i]);
+    int **mat = (int **)malloc(rows * sizeof(int*));
 
-    return m;
+    for(int i = 0; i < rows; i++)
+        mat[i] = (int *)malloc(cols * sizeof(int));
+
+    return mat;
 }
 
 
 int main(int argc, char **argv){
-	int myid, numprocs;
+	int procid, numprocs;
 	int **a, **b, **r;      // matrices
 	int i, j, k;            // counters
     int m = 5, n = 2, p = 4;
+    int taskSize, taskLines, lineCounter, numLines;
 
-	/*   A          B               C
-	   00 01			          00 01 02 03
-       10 11	 00 01 02 03	  10 11 12 13
-       20 21	 10 11 12 13	  20 21 22 23
-       30 31                      30 31 32 33
-       40 41                      40 41 42 43
-
-       P0 = MASTER
-       P1 <- 00, 10
-       P2 <- 20, 30
-       P3 <- 40
-
-    */
-
+	MPI_Status status;
 	MPI_Init(&argc, &argv);
 	MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
-	MPI_Comm_rank(MPI_COMM_WORLD, &myid);
+	MPI_Comm_rank(MPI_COMM_WORLD,&procid);
 
-	if(myid == MASTER){     /* Matrix TEST data initialization */
+	if(procid == MASTER){     /* Matrix TEST data initialization */
         int m = 3, n = 2, p = 4;
         /* Allocate data per matrix*/
         a = alloc_matrix(m, n);
@@ -57,7 +45,7 @@ int main(int argc, char **argv){
                 b[i][j] = rand() % 10;
 	}
 
-    if(myid == MASTER){    /* MASTER process */
+    if(procid == MASTER){    /* MASTER process */
         taskSize = (n + ((numprocs-1) / 2 )) / (numprocs-1);    // maximum tasks per process
         lineCounter = 0;
         for(i=1; i<numprocs; i++) {
@@ -86,19 +74,20 @@ int main(int argc, char **argv){
         }
     }
 
-    if(myid != MASTER) {
+    if(procid != MASTER) {
         MPI_Recv(&lineCounter, 1, MPI_INT, MASTER, 1, MPI_COMM_WORLD, &status);     //end at
         MPI_Recv(&taskLines, 1, MPI_INT, MASTER, 1, MPI_COMM_WORLD, &status);
         MPI_Recv(&(a[lineCounter-numLines][0]), taskLines*n, MPI_INT, MASTER, 1, MPI_COMM_WORLD, &status);
         MPI_Recv(&(b[0][0]), n*p, MPI_INT, MASTER, 1, MPI_COMM_WORLD, &status);
 
-        for(i=lineCounter-taskLines; i<lineCounter, i++)
+        for(i=lineCounter-taskLines; i<lineCounter; i++)
             for(j=0; j<p; j++)
                 for(k = 0; k < n; ++k)
                     r[i][j] += a[i][k] * b[k][j];
 
         MPI_Send(&taskLines , 1, MPI_INT, i, MASTER, MPI_COMM_WORLD);
         MPI_Send(&(r[lineCounter-taskLines][0]) , taskLines*p, MPI_INT, MASTER, 1, MPI_COMM_WORLD);
+    }
 
 	MPI_Finalize();
 }

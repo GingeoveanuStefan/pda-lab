a15da62dd1d89483b4a6f13f1d4573a77933cf8b
GingeoveanuStefan <stefan.razv@gmail.com>
2017-04-10 23:54:15 +0300

    Add files via upload

--- TEME/MPI/lab2 - array search/source.cpp
+++ TEME/MPI/lab2 - array search/source.cpp
@@ -1,4 +1,4 @@
-#include "mpi.h")
+#include "mpi.h"
 #include <stdio.h>
 #include <math.h>
 
@@ -9,34 +9,17 @@
 int main(int argc, char **argv){
 	int myid, numprocs;
 
-	int M1[SIZE] = {(1,2,3),(4,5,6),(7,8,9)};
-	int M2[SIZE] = {(9,8,7),(6,5,4),(3,2,1)};
-	int target = TARGET;
-	int i, j, x, low, high, myresult=0, result;
+	int data[SIZE] = {5,2,3,4,5,6,7,8}, target=TARGET;
+	int i, x, low, high, myresult=0, result;
 
-	//FILE *fp;
+	FILE *fp;
 
 	MPI_Init(&argc, &argv);
 	MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
 	MPI_Comm_rank(MPI_COMM_WORLD, &myid);
 
-
-	//if(MASTER == myid) {
-		/* open input file and intialize data */
-	//	if( NULL == (fp = fopen("input.txt", "r")) ) {
-	//		printf("\nCan't open the input file.");
-	//		MPI_Abort;
-	//	} else
-		//for(i=0; i<MAXSIZE; i++) {
-	//		printf("\nFile opened.");
-			//fscanf(fp, "%d", &data[i]);
-		//}
-	//}
-
-
-
 	/* broadcast data */
-	MPI_Bcast(M1, SIZE, MPI_INT, 0, MPI_COMM_WORLD);
+	MPI_Bcast(data, SIZE, MPI_INT, 0, MPI_COMM_WORLD);
 
 	/* add portion of data */
 	x = SIZE/numprocs; /* must be an integer */
@@ -44,20 +27,17 @@ int main(int argc, char **argv){
 	high = low + x;
 
 	for(i=low; i<high; i++) {
-		//if(M1[i] == target)
-			//myresult += i;
-		for(j=i; j<high; j++)
-			printf("\nI got %d from %d", M1[i][j], myid);
+		if(data[i] == target)
+			myresult += i;
 	}
 
-	//printf("\nI got %d from %d", myresult, myid);
+	printf("\nI got %d from %d", myresult, myid);
 
-	/* compute global sum */
-	//MPI_Reduce(&myresult, &result, 1, MPI_INT, MPI_MAX, 0, MPI_COMM_WORLD);
+	MPI_Reduce(&myresult, &result, 1, MPI_INT, MPI_MAX, 0, MPI_COMM_WORLD);
 
-	//if(0 == myid) {
-	//	printf("\nElement found at position %d.", result);
-	//}
+	if(0 == myid) {
+		printf("\nElement found at position %d.", result);
+	}
 
 	MPI_Finalize();
 }
